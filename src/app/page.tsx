e17af31b15648225d950c5343a2c8868ceeba3d3
Davis Joshi <davis.abragam@ei.study>
2026-01-26 18:00:34 +0530

    Setting up neon database and configured drizzle ORM.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className='text-4xl font-bold text-green-500'>Hello world</div>
+    <Button variant="destructive">click me</Button>
   );
 }
